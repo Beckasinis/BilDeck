03695ec8562746c425ee6dd3cf6682f446f56feb
Annelie <annelie.j.rova@gmail.com>
2026-04-27 00:04:29 +0200

    feat(routing): initialize app structure with header, footer and routes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,13 @@
+import './header.css';
+import Hamburger from './Hamburger'
+import LoginModal from '../loginModal'
+
 function Header(){
     return (
         <>
         <h1>Header</h1>
+        <Hamburger />
+        <LoginModal />
         </>
     );
 }
--- src/views/login/index.js
+++
@@ -1,5 +0,0 @@
-export { default } from './login.jsx';
-
-// Re-export everything from this folder here.
-// This allows importing from 'views/login' instead of 'views/login/login.jsx'
-// Example: import Login from './views/login'
