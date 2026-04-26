fb2ec4c965eff8b21cb4199c37753ef9127a6816
Annelie <annelie.j.rova@gmail.com>
2026-04-26 16:17:02 +0200

    feat(views): add boilerplate, view components

---
+++ src/views/home/home.jsx
@@ -0,0 +1,10 @@
+function HomeView(){
+    return (
+        <>
+        <h1>HOME</h1>
+        <p>Här kommer info om sidan</p>
+        </>
+    );
+}
+
+export default HomeView;
---
+++ src/views/login/login.jsx
@@ -0,0 +1,10 @@
+function LoginView(){
+    return (
+        <>
+        <h1>Login</h1>
+        <p>Här kommer login sidan</p>
+        </>
+    );
+}
+
+export default LoginView;
