b98aa8b209737f7c1f845f1bd83d482327d3a528
Beckasinis <beccis22@hotmail.com>
2026-05-10 18:52:47 +0200

    feat(notfound): new design of 404-page

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,10 +1,14 @@
-import './notfound.css';
+import './notfound.css'
 
 function NotFoundView() {
   return (
     <div className="notfound-view">
-      <h1>404</h1>
-      <p>Sidan finns inte</p>
+      <div className="notfound-card">
+        <img src="public/img/apple-touch-icon.png" alt="Logo" />
+        <h1 className="notfound-code">404</h1>
+        <h2 className="notfound-title">Hoppsan, nu har du sladdat fel.</h2>
+        <p className="notfound-text">Sidan du letar efter verkar inte existera eller har flyttats.</p>
+      </div>
     </div>
   )
 }
